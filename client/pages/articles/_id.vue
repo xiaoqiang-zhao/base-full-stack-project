6a23035186fc82cf44233ec8cae3ac746e9e2c81
xiaoqiang-zhao <longze_xq@163.com>
2018-08-25 07:58:25 +0800

    添加首页转发功能，添加导航逻辑

---
+++ client/pages/articles/_id.vue
@@ -0,0 +1,71 @@
+<template>
+    <section class="container article-detail">
+        文章详情页
+    </section>
+</template>
+<script>
+/**
+ * @file 文章列表页
+ * @author 小强赵
+ */
+import elTable from 'element-ui/lib/table';
+import elTableColumn from 'element-ui/lib/table-column';
+import 'element-ui/lib/theme-default/table.css';
+import 'element-ui/lib/theme-chalk/icon.css';
+import elButton from 'element-ui/lib/button';
+import 'element-ui/lib/theme-chalk/button.css';
+import messageBox from 'element-ui/lib/message-box';
+import 'element-ui/lib/theme-chalk/message-box.css';
+import message from 'element-ui/lib/message';
+import 'element-ui/lib/theme-chalk/message.css';
+import elForm from 'element-ui/lib/form';
+import 'element-ui/lib/theme-chalk/form.css';
+import elFormItem from 'element-ui/lib/form-item';
+import 'element-ui/lib/theme-chalk/form-item.css';
+import elInput from 'element-ui/lib/input';
+
+import axios from '@/../plugins/axios';
+import utiles from '@/../utiles';
+
+export default {
+    components: {
+        elTable,
+        elTableColumn,
+        elButton,
+        elForm,
+        elFormItem,
+        elInput
+    },
+    // async asyncData() {
+    //     let tableData;
+    //     try {
+    //         const res = await axios.get('/api/articles');
+    //         tableData = res.data;
+    //     }
+    //     catch (e) {
+    //         tableData = [];
+    //     }
+
+    //     return {
+    //         tableData
+    //     };
+    // },
+    data() {
+        return {
+        };
+    },
+    methods: {
+
+        /**
+         * 添加一条文章数据
+         */
+        addArticle() {
+        }
+    }
+};
+</script>
+<style lang="less" scoped>
+.article-detail {
+    
+}
+</style>
---
+++ client/pages/articles/index.vue
@@ -0,0 +1,169 @@
+<template>
+    <section class="container article-list">
+        <!-- 头部按钮行 -->
+        <header class="button-line">
+            <el-button
+                type="primary"
+                icon="el-icon-circle-plus-outline"
+                @click="toDetail(0, 1)">
+                新增文章
+            </el-button>
+        </header>
+        <!-- 文章列表 -->
+        <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                label="文章标题"
+                prop="title"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                label="创建时间"
+                prop="createDate"
+                :formatter="dateFormatter">
+            </el-table-column>
+            <el-table-column
+                label="更新时间"
+                prop="updateDate"
+                :formatter="dateFormatter">
+            </el-table-column>
+            <el-table-column
+                label="操作"
+                width="100">
+                <template slot-scope="scope">
+                    <el-button @click="deleteArticle(scope.row)" type="text" size="small">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </section>
+</template>
+<script>
+/**
+ * @file 文章列表页
+ * @author 小强赵
+ */
+import elTable from 'element-ui/lib/table';
+import elTableColumn from 'element-ui/lib/table-column';
+import 'element-ui/lib/theme-default/table.css';
+import 'element-ui/lib/theme-chalk/icon.css';
+import elButton from 'element-ui/lib/button';
+import 'element-ui/lib/theme-chalk/button.css';
+import messageBox from 'element-ui/lib/message-box';
+import 'element-ui/lib/theme-chalk/message-box.css';
+import message from 'element-ui/lib/message';
+import 'element-ui/lib/theme-chalk/message.css';
+import elForm from 'element-ui/lib/form';
+import 'element-ui/lib/theme-chalk/form.css';
+import elFormItem from 'element-ui/lib/form-item';
+import 'element-ui/lib/theme-chalk/form-item.css';
+import elInput from 'element-ui/lib/input';
+
+import axios from '@/../plugins/axios';
+import utiles from '@/../utiles';
+
+export default {
+    components: {
+        elTable,
+        elTableColumn,
+        elButton,
+        elForm,
+        elFormItem,
+        elInput
+    },
+    // async asyncData() {
+    //     let tableData;
+    //     try {
+    //         const res = await axios.get('/api/articles');
+    //         tableData = res.data;
+    //     }
+    //     catch (e) {
+    //         tableData = [];
+    //     }
+
+    //     return {
+    //         tableData
+    //     };
+    // },
+    data() {
+        return {
+            tableData: []
+        };
+    },
+    methods: {
+
+        /**
+         * 到文章详情页
+         *
+         * @param {string} id 文章 ID，新建时 ID 为 0
+         * @param {number} isEdit 是否可编辑，0: 不可编辑只展示，1: 可编辑包括添加新文章
+         */
+        toDetail(id, isEdit) {
+            this.$router.push({
+                path: `/articles/${id}`,
+                query: {
+                    isEdit
+                }
+            });
+
+        },
+
+        /**
+         * 删除文章
+         *
+         * @param {Object} articleItem 一条文章数据
+         */
+        deleteArticle(articleItem) {
+            messageBox.confirm('此操作将永久删除该资源, 是否继续?', '提示', {
+                confirmButtonText: '继续',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => axios.delete('/api/articles/' + articleItem._id)
+            ).then(res => {
+                // 移除页面中的数据
+                this.tableData.some((item, index) => {
+                    if (item._id === articleItem._id) {
+                        this.tableData.splice(index, 1);
+                        return true;
+                    }
+                });
+                message({
+                    type: 'success',
+                    message: '删除成功!'
+                });
+            });
+        },
+
+        /**
+         * 添加一条文章数据
+         */
+        addArticle() {
+        },
+
+        /**
+         * 格式化一条资源数据的两个时间字段：createDate、updateDate
+         *
+         * @param {Object} row 一条资源数据
+         * @param {Object} column 单元格对象
+         * @param {Object} cellValue 字段值
+         *
+         * @return {string} 格式化后的时间
+         */
+        dateFormatter(row, column, cellValue) {
+            return utiles.formatDate(cellValue);
+        }
+    }
+};
+</script>
+<style lang="less" scoped>
+.article-list {
+    .button-line {
+        text-align: right;
+        padding: 5px 0;
+    }
+    .el-form,
+    .dialog-footer {
+        padding-right: 17px;
+    }
+}
+</style>
